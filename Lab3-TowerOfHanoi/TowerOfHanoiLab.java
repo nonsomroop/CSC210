0da69250a3ca3a8a4c28260ca2f23a895944c43a
nonsomroop <somroopnatthanon@gmail.com>
2023-10-11 13:51:11 +0700

    [Edit] format

--- Lab3-TowerOfHanoi/TowerOfHanoiLab.java
+++ Lab3-TowerOfHanoi/TowerOfHanoiLab.java
@@ -1,6 +1,6 @@
 public class TowerOfHanoiLab {
     public static void main(String [] args){
-        TowerOfHanoi t1 = new TowerOfHanoi(30);
+        TowerOfHanoi t1 = new TowerOfHanoi(5);
         t1.play();
     }
 }
